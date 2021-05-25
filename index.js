81d22e5143ad3b28c7731278407f3f947e11b57d
David-V-Rullo <d.v.rullo@gmail.com>
2021-05-25 19:19:44 -0400

    Was able to write all questions needed for readme

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "github",
+    message: "What's your Github username?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What's your email?"
+},
+{
+    type: "input",
+    name: "title",
+    message: "What's the name of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Describe about your project:"
+},
+{
+    type: "list",
+    name: "license",
+    message: "What license does your project have?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2" ,"BSD3", "None"]
+},
+{
+    type: "input",
+    name: "dependencies",
+    message: "Any dependencies to install?",
+    default: "npm i"
+},
+{
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What does the usage of this repo?",
+},
+{
+    type: "input",
+    name: "contributors",
+    message: "Who are the contributors of this repo?",
+}
+
+];];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
